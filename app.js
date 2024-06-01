2a65fc61e14eba1bcd39ee257ec21e8e7a5c425d
Nikita Klochko <mandicis173@gmail.com>
2024-06-01 15:53:49 +0300

    add connection to db

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI)
+		app.listen(port, () =>
+			console.log(`✔️  Server is listening on port ${port}...`),
+		)
+	} catch (error) {
+		console.log('❌ Error occured while starting server', error)
+	}
+}
+
+start()
